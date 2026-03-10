9f6a91128baa406caf518c38a14f83f2a74f5e9a
arungandhis <144406712+arungandhis@users.noreply.github.com>
2026-03-10 12:52:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const autoDetectMatches = require("./autoMatchDetector")
 const express = require("express")
 const http = require("http")
 const cors = require("cors")
@@ -93,4 +95,5 @@ const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   console.log("Server running on port", PORT)
+  autoDetectMatches()
 })
