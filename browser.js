b69a91e59978fe21b018dfa4c4746da7bd7799fc
arungandhis <144406712+arungandhis@users.noreply.github.com>
2026-03-13 07:55:37 -0400

    Create browser.js

---
+++ browser.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+
+let browser = null;
+
+async function getBrowser() {
+
+  if (browser) return browser;
+
+  browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu"
+    ]
+  });
+
+  return browser;
+}
+
+module.exports = { getBrowser };
